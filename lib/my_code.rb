d621e094fb359405ad9875387a498b219b6dfad2
George Apetrei <george.apetrei@gmail.com>
2019-12-24 12:00:35 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -40,9 +40,10 @@ def reduce_to_total(source_array, starting_point)
   counter = 0 
   total= 0
   if counter < source_array.size
-    total +=source_array[counter] + starting_point
+    total +=source_array[counter] 
     counter +=1 
   end
+  total += starting_point
 end
 
 
